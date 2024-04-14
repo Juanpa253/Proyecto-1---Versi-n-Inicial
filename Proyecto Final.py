30bfedd7eeac89534611d49d601963320a93c2b1
Juanpa253 <juanmel.dlrs.0507@gmail.com>
2024-04-14 17:24:40 -0600

    Gestión de Inventarios: Listo

--- Proyecto Final.py
+++ Proyecto Final.py
@@ -1,3 +1,31 @@
+
+class Deposito:
+    def __init__(self,Nombre,CapacidadMax,Almacenado,PrecioAlmacenamiento):
+        self.Nombre = Nombre
+        self.CapacidadMax = CapacidadMax
+        self.Almacenado = Almacenado
+        self.PrecioAlmacenamiento = PrecioAlmacenamiento
+    
+    def MostrarCapacidad (self):
+        print("La cantidad almacenada en el deposito de combustible", self.Nombre, "actualmente es:", self.Almacenado,"Galones")
+
+    def AgregarCombustible (self):
+        CantidadGalones = int(input("Cuántos galones desea agregar? "))
+        if CantidadGalones >= 0:
+            if self.Almacenado + CantidadGalones <= self.CapacidadMax:
+                print("Se agregaron", CantidadGalones, "galones de combustible al deposito", self.Nombre)
+                self.Almacenado = self.Almacenado + CantidadGalones
+                self.MostrarCapacidad()
+            else:
+                print("La cantidad por agregar supera el límite del deposito")
+        else:
+            print("La cantidad ingresada debe ser positiva")
+
+#Crear depósitos
+DepositoRegular = Deposito("Regular", 80, 40, 7) 
+DepositoSuper = Deposito("Super", 80,40, 8) 
+DepositoDiesel = Deposito("Diesel", 80, 40, 6) 
+
 #Menu
 print("Bienvenido a Gasolineras Jaguar")
 print("1. Gestión de Inventario")
@@ -9,11 +37,15 @@ MenuNumero = int(input("ingrese la opcion deseada: "))
 
 #Gestión de Inventario
 if MenuNumero == 1:
-    #Función por implementar, se mostrará el nivel de los depósitos.
-    RellenarDepositos = input("Desea agregar gasolina a los depósitos ? Sí/No ")
-    if RellenarDepositos == "Sí":
-        #La función para rellenar depósitos va aquí
-
+    DepositoRegular.MostrarCapacidad()
+    DepositoSuper.MostrarCapacidad()
+    DepositoDiesel.MostrarCapacidad()
+    #Agregar Gasolina
+    RellenarDepositos = input("Desea agregar gasolina a los depósitos ? Si/No ").lower()
+    if RellenarDepositos == "si":
+        DepositoRegular.AgregarCombustible()
+        DepositoSuper.AgregarCombustible()
+        DepositoDiesel.AgregarCombustible()
 
 #Venta de Combustible
 elif MenuNumero == 2:
@@ -23,11 +55,14 @@ elif MenuNumero == 2:
     print("3. Diesel")
     TipodeCombustibleVenta = int(input("Elija el número del tipo de combustible por vender: "))
     if TipodeCombustibleVenta == 1:
+        pausa = 0
         #Vender Regular
     elif TipodeCombustibleVenta == 2:
+        pausa = 0
         #Vender Super
     elif TipodeCombustibleVenta == 3:
         #Vender Diesel
+        pausa = 0
 
 
 #Gestion de Turnos
@@ -40,10 +75,12 @@ elif  MenuNumero == 3:
     print(" Salario Total Jornada Diurna: ")
     print(" Salario Total Jornada Vespertina: ")
     print(" Salario Total Jornada Nocturna: ")
+    
 
 #Reporte de Rentabilidad
 elif MenuNumero == 4:
     #Función por implementar, gastos totales.
+    pausa = 0
 
 #Salida
 elif MenuNumero == 5:
