cca9ca68f61c6dda5ed52c8cfde12878747f42db
Juanpa253 <juanmel.dlrs.0507@gmail.com>
2024-04-18 00:59:21 -0600

    Proyecto Finalizado

--- Proyecto Final.py
+++ Proyecto Final.py
@@ -3,8 +3,9 @@ class Deposito:
         self.Nombre = Nombre
         self.CapacidadMax = CapacidadMax
         self.Almacenado = Almacenado
-        self.PrecioAlmacenamiento = PrecioAlmacenamiento
+        self.CostoAlmacenamiento = PrecioAlmacenamiento
         self.PrecioVenta = PrecioVenta
+        self.GalonesVendidos = 0
     
     def MostrarCapacidad (self):
         print("La cantidad almacenada en el deposito de combustible", self.Nombre, "actualmente es:", self.Almacenado,"Galones")
@@ -53,10 +54,19 @@ class Deposito:
                     print("--------------------------------------------------------------")
                     print("")
                     self.Almacenado = self.Almacenado - GalonesVender
+                    self.GalonesVendidos = self.GalonesVendidos + GalonesVender
                 else:
                     print("Ingrese una bomba valida.")
         else:
             print("Su respuesta no es valida.")
+    
+    def CalcularVentas (self):
+        ValorVentas = self.GalonesVendidos * self.PrecioVenta
+        return ValorVentas
+    
+    def CalcularCostoAlmacenamiento (self):
+        ValorAlmacenamiento = self.Almacenado * self.CostoAlmacenamiento
+        return ValorAlmacenamiento
 
 class Turno:
     def __init__(self, Jornada, CantidadTrabajadores, SalarioPorHora):
@@ -100,79 +110,108 @@ DepositoRegular = Deposito("Regular", 80, 40, 7, 29)
 DepositoSuper = Deposito("Super", 80,40, 8, 30) 
 DepositoDiesel = Deposito("Diesel", 80, 40, 6, 26.50) 
 
-#Menu
-print("Bienvenido a Gasolineras Jaguar")
-print("1. Gestion de Inventario")
-print("2. Venta de Combustible")
-print("3. Gestion de Turnos")
-print("4. Reporte de Rentabilidad")
-print("5. Salida")
-MenuNumero = int(input("ingrese la opcion deseada: "))
+Continuar = True
+while Continuar == True:
+    #Menu
+    print("Bienvenido a Gasolineras Jaguar")
+    print("1. Gestion de Inventario")
+    print("2. Venta de Combustible")
+    print("3. Gestion de Turnos")
+    print("4. Reporte de Rentabilidad")
+    print("5. Salida")
+    MenuNumero = int(input("ingrese la opcion deseada: "))
 
-#Gestión de Inventario
-if MenuNumero == 1:
-    DepositoRegular.MostrarCapacidad()
-    DepositoSuper.MostrarCapacidad()
-    DepositoDiesel.MostrarCapacidad()
-    #Agregar Gasolina
-    RellenarDepositos = input("Desea agregar combustible a los depositos ? Si/No ").lower()
-    if RellenarDepositos == "si":
-        DepositoRegular.AgregarCombustible()
-        DepositoSuper.AgregarCombustible()
-        DepositoDiesel.AgregarCombustible()
+    #Gestión de Inventario
+    if MenuNumero == 1:
+        DepositoRegular.MostrarCapacidad()
+        DepositoSuper.MostrarCapacidad()
+        DepositoDiesel.MostrarCapacidad()
+        #Agregar Gasolina
+        RellenarDepositos = input("Desea agregar combustible a los depositos ? Si/No ").lower()
+        if RellenarDepositos == "si":
+            DepositoRegular.AgregarCombustible()
+            DepositoSuper.AgregarCombustible()
+            DepositoDiesel.AgregarCombustible()
 
-#Venta de Combustible
-elif MenuNumero == 2:
-    DepositoRegular.MostrarValorCombustible()
-    DepositoSuper.MostrarValorCombustible()
-    DepositoDiesel.MostrarValorCombustible()
-    #Vender
-    VenderCombustible = input("Desea vender combustible ? Si/No ").lower()
-    if VenderCombustible == "si":
-        print("1. Regular")
-        print("2. Super")
-        print("3. Diesel")
-        TipodeCombustibleVenta = int(input("Elija el numero del tipo de combustible por vender: "))
-        if TipodeCombustibleVenta == 1:
-            #Vender Regular
-            DepositoRegular.VentaCombustible()
-        elif TipodeCombustibleVenta == 2:
-            #Vender Super
-            DepositoSuper.VentaCombustible()
-        elif TipodeCombustibleVenta == 3:
-            #Vender Diesel
-            DepositoDiesel.VentaCombustible()
+    #Venta de Combustible
+    elif MenuNumero == 2:
+        DepositoRegular.MostrarValorCombustible()
+        DepositoSuper.MostrarValorCombustible()
+        DepositoDiesel.MostrarValorCombustible()
+        #Vender
+        VenderCombustible = input("Desea vender combustible ? Si/No ").lower()
+        if VenderCombustible == "si":
+            print("1. Regular")
+            print("2. Super")
+            print("3. Diesel")
+            TipodeCombustibleVenta = int(input("Elija el numero del tipo de combustible por vender: "))
+            if TipodeCombustibleVenta == 1:
+                #Vender Regular
+                DepositoRegular.VentaCombustible()
+            elif TipodeCombustibleVenta == 2:
+                #Vender Super
+                DepositoSuper.VentaCombustible()
+            elif TipodeCombustibleVenta == 3:
+                #Vender Diesel
+                DepositoDiesel.VentaCombustible()
 
 
-#Gestion de Turnos
-elif  MenuNumero == 3:
-    JornadaDiurna.MostrarCantidadTrabajadores()
-    JornadaNocturna.MostrarCantidadTrabajadores()
-    JornadaVespertina.MostrarCantidadTrabajadores()
-    print("Modificación de jornadas: Agregar trabajadores")
-    JornadaDiurna.AgregarTrabajadores()
-    JornadaVespertina.AgregarTrabajadores()
-    JornadaNocturna.AgregarTrabajadores()
-    print("Modificación de jornadas: Retirar trabajadores")
-    JornadaDiurna.RetirarTrabajadores()
-    JornadaVespertina.RetirarTrabajadores()
-    JornadaNocturna.RetirarTrabajadores()
-    #Costo trabajadores
-    SalarioJornada = JornadaDiurna.CalcularSalario()
-    print("El salario total de la jornada diurna es de Q", SalarioJornada )
-    SalarioJornada = JornadaVespertina.CalcularSalario()
-    print("El salario total de la jornada vespertina es de Q", SalarioJornada  )
-    SalarioJornada = JornadaNocturna.CalcularSalario()
-    print("El salario total de la jornada nocturna es de Q", SalarioJornada  )
-  
-#Reporte de Rentabilidad
-elif MenuNumero == 4:
-    #Función por implementar, gastos totales.
-    pausa = 0
+    #Gestion de Turnos
+    elif  MenuNumero == 3:
+        JornadaDiurna.MostrarCantidadTrabajadores()
+        JornadaNocturna.MostrarCantidadTrabajadores()
+        JornadaVespertina.MostrarCantidadTrabajadores()
+        print("Modificación de jornadas: Agregar trabajadores")
+        JornadaDiurna.AgregarTrabajadores()
+        JornadaVespertina.AgregarTrabajadores()
+        JornadaNocturna.AgregarTrabajadores()
+        print("Modificación de jornadas: Retirar trabajadores")
+        JornadaDiurna.RetirarTrabajadores()
+        JornadaVespertina.RetirarTrabajadores()
+        JornadaNocturna.RetirarTrabajadores()
+        #Costo trabajadores
+        SalarioJornada = JornadaDiurna.CalcularSalario()
+        print("El salario total de la jornada diurna es de Q", SalarioJornada )
+        SalarioJornada = JornadaVespertina.CalcularSalario()
+        print("El salario total de la jornada vespertina es de Q", SalarioJornada  )
+        SalarioJornada = JornadaNocturna.CalcularSalario()
+        print("El salario total de la jornada nocturna es de Q", SalarioJornada  )
+    
+    #Reporte de Rentabilidad
+    elif MenuNumero == 4:
+        IngresosTotales = DepositoRegular.CalcularVentas() + DepositoSuper.CalcularVentas() + DepositoDiesel.CalcularVentas()
+        MateriaPrima = DepositoRegular.CalcularCostoAlmacenamiento() + DepositoSuper.CalcularCostoAlmacenamiento() + DepositoDiesel.CalcularCostoAlmacenamiento()
+        ManoDeObra = JornadaDiurna.CalcularSalario() + JornadaVespertina.CalcularSalario() + JornadaNocturna.CalcularSalario()
+        CostosFijos = 10
+        UtilidadBruta = IngresosTotales - MateriaPrima - ManoDeObra - CostosFijos
 
-#Salida
-elif MenuNumero == 5:
-    print("El programa ha terminado, gracias por preferirnos.")
+        CostoCombustibleRegular = DepositoRegular.CalcularCostoAlmacenamiento()
+        CostoCombustibleSuper = DepositoSuper.CalcularCostoAlmacenamiento()
+        CostoCombustibleDiesel = DepositoDiesel.CalcularCostoAlmacenamiento()
 
-else:
-    print("Ingrese una opcion valida")
+        SalarioJornadaDiurna = JornadaDiurna.CalcularSalario()
+        SalarioJornadaVespertina = JornadaVespertina.CalcularSalario()
+        SalarioJornadaNocturna = JornadaNocturna.CalcularSalario()
+
+        print("")
+        print("--------------------------------------------------------------")
+        print("Ingresos Totales:                   Q", IngresosTotales)
+        print("Materia Prima:")
+        print("Costo Combustible Regular:          Q",CostoCombustibleRegular )
+        print("Costo Combustible Super:            Q",CostoCombustibleSuper )
+        print("Costo Combustible Diesel:           Q",CostoCombustibleDiesel )
+        print("Mano de obra:")
+        print("Salario Jornada Diurna:             Q",SalarioJornadaDiurna )
+        print("Salario Jornada Vespertina:         Q",SalarioJornadaVespertina )
+        print("Salario Jornada Nocturna:           Q",SalarioJornadaNocturna)
+        print("Costos Fijos:                       Q",CostosFijos)
+        print("--------------------------------------------------------------")
+        print("Utilidad Bruta:                     Q",UtilidadBruta)
+
+    #Salida
+    elif MenuNumero == 5:
+        print("El programa ha terminado, gracias por preferirnos.")
+        Continuar = False
+
+    else:
+        print("Ingrese una opcion valida")
